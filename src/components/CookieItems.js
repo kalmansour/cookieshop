75f1faf46839307cc6f74c5afa922637a95ee3d7
kalmansour <khaledalmansour24@gmail.com>
2020-11-05 14:47:27 +0300

    Done search bar task & challenge

--- src/components/CookieItems.js
+++ src/components/CookieItems.js
@@ -1,13 +1,11 @@
 import { CookieWrapper } from "../styles";
 const CookieItem = (props) => {
   const cookie = props.cookie;
-  console.log(props);
   return (
     <CookieWrapper key={cookie.id}>
       <img src={cookie.image} alt={cookie.name} />
       <p>{cookie.name}</p>
       <p className="cookie-price">{cookie.price} KWD</p>
-      <p>{}</p>
     </CookieWrapper>
   );
 };
